e8f27fb2eec5f13efc22e5852f86a789cc04f061
Sylvain Carisey <sylvain@carisey.dev>
2025-12-05 11:29:30 +0100

    chore:update flake

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "SCarisey's homelab module";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.11";
   };
 
   outputs = {
